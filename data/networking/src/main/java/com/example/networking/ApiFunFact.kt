0479d50d6bf531e3ce4d0704bac412a15a58c0bc
andresserranoABI <andasevi@quilmes.com.ar>
2022-09-15 16:36:34 -0500

    merge fixes

--- data/networking/src/main/java/com/example/networking/ApiFunFact.kt
+++
@@ -1,5 +0,0 @@
-package com.example.networking
-
-internal class ApiFunFact : FunFactNetworking {
-    override fun getRandomFunFact(): String = "A FULL NASA SPACE SUIT COSTS $12,000,000."
-}
--- data/src/main/java/com/example/data/FunFactRepositoryImpl.kt
+++ data/src/main/java/com/example/data/FunFactRepositoryImpl.kt
@@ -3,9 +3,9 @@ package com.example.data
 import com.example.domain.FunFactRepository
 import com.example.local.FunFactLocal
 import com.example.networking.FunFactNetworking
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
+
 import kotlin.random.Random
+import com.example.networking.utils.Status
 
 
 class FunFactRepositoryImpl constructor(
@@ -13,15 +13,20 @@ class FunFactRepositoryImpl constructor(
     private val funFactLocal: FunFactLocal
 ) : FunFactRepository {
     override suspend fun getRandomFunFact(hasConnection: Boolean): String {
-        return withContext(Dispatchers.IO) {
-            if (hasConnection) {
-                val response = funFactNetworking.getRandomFunFact()
-                funFactLocal.saveFunFact(response)
-                return@withContext "API $response"
-            } else {
-                val localData =  funFactLocal.getFunFact()
-                return@withContext "DB ${localData[Random.nextInt(localData.size)]}"
+        return if (hasConnection) {
+            val remoteFunFact = funFactNetworking.getRandomFunFact()
+            return when (remoteFunFact.status) {
+                Status.SUCCESS ->{
+                    val responseData = remoteFunFact.data?.value?.fact.toString()
+                    funFactLocal.saveFunFact(responseData)
+                    responseData
+                }
+                Status.ERROR -> remoteFunFact.message.toString()
+                Status.LOADING -> "Loading..."
             }
+        } else {
+            val localData =  funFactLocal.getFunFact()
+            localData[Random.nextInt(localData.size)]
         }
     }
 }
