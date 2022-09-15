613fd99e6791bd33aab6c1f4821a7bf007cacae9
andresserranoABI <andasevi@quilmes.com.ar>
2022-09-15 12:42:59 -0500

    Room Implementation (codelab) [https://developer.android.com/codelabs/android-room-with-a-view-kotlin#17]

--- data/local/src/main/java/com/example/local/di/localModule.kt
+++ data/local/src/main/java/com/example/local/di/localModule.kt
@@ -1,9 +1,20 @@
 package com.example.local.di
 
+import androidx.room.Room
 import com.example.local.FunFactLocal
 import com.example.local.FunFactLocalImpl
+import com.example.local.room.FunFactDatabase
+import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val localModule = module {
-    single<FunFactLocal> { FunFactLocalImpl() }
+    single<FunFactLocal> { FunFactLocalImpl(get()) }
+    single {
+        Room.databaseBuilder(
+            androidContext(),
+            FunFactDatabase::class.java,
+            "fun_fact_database"
+        ).fallbackToDestructiveMigration().build()
+    }
+    single { get<FunFactDatabase>().funFactDao() }
 }
